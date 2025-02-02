d7d80b28f7e0254a92cf140c514721aa16be54bb
adjaballah <155402021+adjaballah@users.noreply.github.com>
2025-02-02 23:39:46 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,9 @@
 import mysql.connector
-from mysql.connector import Error
+from mysql. connector import Error
 
 def create_database():
     try:
-        
+       
         connection = mysql.connector.connect(
             host='localhost',  # Replace with your MySQL server address if different
             user='your_user',  # Replace with your MySQL username
